cd34137c0d68d7ef040a59997910d6f56a4c4232
daina-r <36935561+daina-r@users.noreply.github.com>
2018-04-16 23:48:29 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,7 @@
+<form enctype="multipart/form-data" action="/index.php" method="POST">
+Введите имя: <input name="fio" type="text" placeholder="Ваше имя">
+<br />
+Загрузить тест: <input name="userfile" type="file" />
+<br />
+<input type="submit" value="Отправить">
+</form>
---
+++ test.php
@@ -0,0 +1,21 @@
+
+
+
+<h3>Пример теста</h3>
+<form action="" method="POST">
+  <fieldset>
+    <legend>Сколько граммов в одном килограмме?</legend>
+    <label><input type="radio" name="q1"> 10</label>
+    <label><input type="radio" name="q1"> 100</label>
+    <label><input type="radio" name="q1"> 1000</label>
+    <label><input type="radio" name="q1"> 10000</label>
+  </fieldset>
+  <fieldset>
+    <legend>Сколько метров в одном дециметре?</legend>
+    <label><input type="radio" name="q2"> 100</label>
+    <label><input type="radio" name="q2"> 10</label>
+    <label><input type="radio" name="q2"> 0.1</label>
+    <label><input type="radio" name="q2"> 0.01</label>
+  </fieldset>
+  <input type="submit" value="Отправить">
+</form>
